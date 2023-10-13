8fae5e55e309d86cf43ce1b2df4ef5f001273eef
ankit-singhal-ac <141702268+ankit-singhal-ac@users.noreply.github.com>
2023-10-13 23:18:38 +0530

    Update HelloWorld.java

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -2,6 +2,9 @@ package oata;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        for (int i = 0; i <= a.length; i++) { // BAD
+              sum += a[i];
+            }
         System.out.println("Hello World");
     }
 }
