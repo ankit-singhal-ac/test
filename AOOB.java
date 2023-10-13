8e855f3233c9208fffb5cfff625b95680cafbc8e
ankit-singhal-ac <141702268+ankit-singhal-ac@users.noreply.github.com>
2023-10-13 22:57:48 +0530

    Update AOOB.java

--- AOOB.java
+++ AOOB.java
@@ -1,7 +1,5 @@
 public class AOOB {
-    public static void main(String args[]){
-
-        System.out.println("Hello World");
-
+     public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
